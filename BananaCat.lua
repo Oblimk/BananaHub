41119e4769318c76f663d1b37057778927e477bd
Oblimk <obliobli525@gmail.com>
2024-12-24 20:08:02 -0300

    Create BananaCat.lua

---
+++ BananaCat.lua
@@ -0,0 +1,4 @@
+local UniverseID = game:GetService("HttpService"):JSONDecode(game:HttpGet("https://apis.roblox.com/universes/v1/places/"..game.PlaceId.."/universe")).universeId
+elseif game.PlaceId == 7449423635 or game.PlaceId == 2753915549 or game.PlaceId == 4442272183 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Oblimk/BananaHub/refs/heads/main/BF-BananaHub.lua"))()
+end
