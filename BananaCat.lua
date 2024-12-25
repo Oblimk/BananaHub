47d711f29cce8234860ee009fbadde878ebe4a6c
Oblimk <obliobli525@gmail.com>
2024-12-25 16:49:38 -0300

    Update BananaCat.lua

--- BananaCat.lua
+++ BananaCat.lua
@@ -2,3 +2,6 @@ local UniverseID = game:GetService("HttpService"):JSONDecode(game:HttpGet("https
 elseif game.PlaceId == 7449423635 or game.PlaceId == 2753915549 or game.PlaceId == 4442272183 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Oblimk/BananaHub/refs/heads/main/BF-BananaHub.lua"))()
 end
+elseif game.PlaceId == 16732694052 then 
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Oblimk/BananaHub/refs/heads/main/Fisch.lua"))()
+end
