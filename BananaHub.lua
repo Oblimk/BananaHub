23597f1d27c0b7baf0873f6eb10a77544a0d2986
Oblimk <obliobli525@gmail.com>
2024-12-26 14:50:07 -0300

    Update BananaHub.lua

--- BananaHub.lua
+++ BananaHub.lua
@@ -6,7 +6,7 @@ if game.PlaceId == 1537690962 or game.PlaceId == 4079902982 then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/Oblimk/BananaHub/refs/heads/main/BSS-Banana.lua"))()
     end
 elseif game.PlaceId == 10260193230 then 
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/Seahuhu-BananaCat.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Oblimk/BananaHub/refs/heads/main/seahuhu-banana.lua"))()
 elseif game.PlaceId == 7449423635 or game.PlaceId == 2753915549 or game.PlaceId == 4442272183 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Oblimk/BananaHub/refs/heads/main/BF-BananaHub.lua"))()
 elseif game.PlaceId == 4520749081 or  game.PlaceId == 6381829480 or game.PlaceId == 15759515082 or game.PlaceId == 5931540094 then 
@@ -18,5 +18,5 @@ elseif game.PlaceId == 4520749081 or  game.PlaceId == 6381829480 or game.PlaceId
 elseif game.PlaceId == 16732694052 then 
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Oblimk/BananaHub/refs/heads/main/Fisch.lua"))()
 elseif UniverseID == 5844593548  then 
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Oblimk/BananaHub/refs/heads/main/seahuhu-banana.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Oblimk/BananaHub/refs/heads/main/AV-Banana%2Clua"))()
 end
