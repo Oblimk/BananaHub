17963b1789cc82f2191264c1e82d6576f81787b4
Oblimk <obliobli525@gmail.com>
2025-01-01 21:54:44 -0300

    Create BananaCat.lua

---
+++ BananaCat.lua
@@ -0,0 +1,3 @@
+repeat wait() until game:IsLoaded() and game.Players.LocalPlayer 
+getgenv().Key = "Input Key"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Oblimk/BananaHub/refs/heads/main/BananaHub.lua"))()
