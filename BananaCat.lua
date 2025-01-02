ada337fb56e4b725624d120ffc47d13b5c15c0d6
Oblimk <obliobli525@gmail.com>
2025-01-02 17:02:10 -0300

    Delete BananaCat.lua

--- BananaCat.lua
+++
@@ -1,3 +0,0 @@
-repeat wait() until game:IsLoaded() and game.Players.LocalPlayer 
-getgenv().Key = "Input Key"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Oblimk/BananaHub/refs/heads/main/BananaHub.lua"))()
